fbe2b3789cea016a55af49909155cd6ec1862dfb
Naomi Katz <naykatz@gmail.com>
2019-02-17 08:04:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,6 +9,11 @@ def second_challenge
     },
       :capulet => {
       #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => [ ]
+      }
       }}
 
   
